be6c32c66136c836f761f966b22d34b0e1dc944f
scaredscope <57763110+scaredscope@users.noreply.github.com>
2019-11-14 15:36:22 +0000

    Update order_rank.py

--- order_rank.py
+++ order_rank.py
@@ -19,8 +19,8 @@ ranks = pd.read_csv("CDLR/current_rank.csv")
 
 change = pd.DataFrame({"x":range(12), "y":ranks.loc[:]["Unnamed: 0"]})
 change["z"] = change["y"] - change["x"]
-print(change)
-####
+
+
 ranks = ranks.sort_values(by="r", ascending=False)
 ranks = pd.DataFrame({"t":ranks.loc[:,"t"].loc[:],"r":ranks.loc[:,"r"].loc[:],"c":change.loc[:,"z"].loc[:]})
 
