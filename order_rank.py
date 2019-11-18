ae54af13bf1715414ad1039d8a0584565e58588b
scaredscope <57763110+scaredscope@users.noreply.github.com>
2019-11-18 15:01:45 +0000

    Update order_rank.py

--- order_rank.py
+++ order_rank.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 
 """loads the ranks from csv files"""
-ranks = pd.read_csv("CDLR/current_rank.csv")
+ranks = pd.read_csv("CDLR/current_rank_2.csv")
 ranks = ranks.sort_values(by="r", ascending=False)
-ranks = pd.DataFrame({"t":ranks.loc[:,"t"].loc[:],"r":ranks.loc[:,"r"].loc[:]})
+ranks = pd.DataFrame({"t":ranks.loc[:,"t"].loc[:],"r":ranks.loc[:,"r"].loc[:],"rd":ranks.loc[:,"rd"].loc[:]})
 
 
 """Updates the current ranks csv file"""
@@ -19,11 +19,9 @@ ranks = pd.read_csv("CDLR/current_rank.csv")
 
 change = pd.DataFrame({"x":range(12), "y":ranks.loc[:]["Unnamed: 0"]})
 change["z"] = change["y"] - change["x"]
-
-
 ranks = ranks.sort_values(by="r", ascending=False)
-ranks = pd.DataFrame({"t":ranks.loc[:,"t"].loc[:],"r":ranks.loc[:,"r"].loc[:],"c":change.loc[:,"z"].loc[:]})
+ranks = pd.DataFrame({"t":ranks.loc[:,"t"].loc[:],"r":ranks.loc[:,"r"].loc[:],"c":change.loc[:,"z"].loc[:],"rd":ranks.loc[:,"rd"].loc[:]})
 
 
 """Updates the current ranks csv file"""
-ranks.to_csv("CDLR/current_rank.csv")
+ranks.to_csv("CDLR/current_rank_2.csv")
